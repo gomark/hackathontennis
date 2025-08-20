ae74af41e05d8c911c1e32dcd35128edde8950ef
Putti <putti.tang@gmail.com>
2025-08-20 09:50:07 +0700

    first commit

---
+++ src/main/java/org/noxnox/hackathontennis/HackathontennisApplication.java
@@ -0,0 +1,14 @@
+package org.noxnox.hackathontennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathontennisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathontennisApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/org/noxnox/hackathontennis/HackathontennisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.noxnox.hackathontennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathontennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
